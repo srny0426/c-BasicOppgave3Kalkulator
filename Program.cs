060dde4468cb57363cf1888631c75a644c0b5bcc
srny0426 <srny0426@gmail.com>
2026-06-19 11:05:02 +0200

    Added while loop, ability to exit program

--- Program.cs
+++ Program.cs
@@ -4,29 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Clear();
-        // User inputs the full calculation example: 1 + 1
-        Console.WriteLine("Input calculation: ");
-        string calculation = Console.ReadLine()!;
+        while (true)
+        {
+            Console.Clear();
+            // User inputs the full calculation example: 1 + 1
+            Console.WriteLine("Input calculation: ");
+            string calculation = Console.ReadLine()!;
 
-        // Finds the position of the operator in the input-string
-        int operatorIndex = calculation.IndexOfAny(['+', '-', '*', '/', '%']);
+            // Finds the position of the operator in the input-string
+            int operatorIndex = calculation.IndexOfAny(['+', '-', '*', '/', '%']);
 
-        // Splits the user input into two halves, before and after the operator 
-        var firstHalf = calculation[..operatorIndex];
-        var secondHalf = calculation[operatorIndex..];
+            // Splits the user input into two halves, before and after the operator 
+            var firstHalf = calculation[..operatorIndex];
+            var secondHalf = calculation[operatorIndex..];
 
-        // Removes spaces and symbols so the halves can be converted to doubles
-        var firstTrimmed = firstHalf.Trim(" ,+-*/%");
-        var secondTrimmed = secondHalf.Trim(" ,+-*/%");
+            // Removes spaces and symbols so the halves can be converted to doubles
+            var firstTrimmed = firstHalf.Trim(" ,+-*/%");
+            var secondTrimmed = secondHalf.Trim(" ,+-*/%");
 
-        // Turns the strings into usable double numbers for calculation
-        double firstNumber = double.Parse(firstTrimmed);
-        double secondNumber = double.Parse(secondTrimmed);
-        char operation = calculation[operatorIndex];
+            // Turns the strings into usable double numbers for calculation
+            double firstNumber = double.Parse(firstTrimmed);
+            double secondNumber = double.Parse(secondTrimmed);
+            char operation = calculation[operatorIndex];
 
-        // Runs the calculate method and presents the answer in a readable format
-        double result = Calculator.Calculate(firstNumber, secondNumber, operation);
-        Console.WriteLine($"{calculation} = {result}");
+            // Runs the calculate method and presents the answer in a readable format
+            double result = Calculator.Calculate(firstNumber, secondNumber, operation);
+            Console.WriteLine($"{calculation} = {result}");
+
+            // Checks if user wants to continue
+            Console.WriteLine("Do you wish to start a new calculation? 'yes' or 'no' ");
+            string choice = Console.ReadLine()!;
+            if (choice == "no")
+                break;
+        }
+
     }
 }
